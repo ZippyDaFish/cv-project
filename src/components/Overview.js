d079db6e9dbdf32e04bf42622d3dcf84e98bd7dc
ZippyDaFish <andrew.riker21@gmail.com>
2023-03-08 14:30:16 -0500

    Display user input

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,9 +3,27 @@ import React from "react";
 const Overview = (props) => {
     const {info} = props;
     return(
-        <ul>
-           
-        </ul>
+        <div>
+            <div id="generalInfo">
+                <p>{info.nameInput}</p>
+                <p>{info.emailInput}</p>
+                <p>{info.phoneInput}</p>
+            </div>
+            <div id="educationInfo">
+                <p>{info.schoolInput}</p>
+                <p>{info.subjectInput}</p>
+                <p>{info.dateInput}</p>
+            </div>
+            <div id="experienceInfo">
+                <p>{info.companyInput}</p>
+                <p>{info.positionInput}</p>
+                <p>{info.taskInput}</p>
+                <div>
+                    <p>{info.startDateInput}</p>
+                    <p>{info.endDateInput}</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
